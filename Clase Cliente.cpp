ad17de85b54914edc9ac8e48be6e486b6b1469bb
AngDom <160678932+AngDom@users.noreply.github.com>
2024-02-25 12:54:06 -0600

    comentario

---
+++ Clase Cliente.cpp
@@ -0,0 +1,20 @@
+#include "Persona.cpp"
+#include <iostream>
+using namespace std;
+
+class Cliente : Persona {
+	// atributos 
+	private : string nit;
+	
+	public: 
+	Cliente (){
+	}
+	
+	Cliente (string nit,){
+    nit=n;
+    
+	}
+	void setNit(string n){nit=n; }
+	}				
+};
+
