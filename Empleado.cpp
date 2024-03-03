65a4b23b91b6ae746f1faf66f4ee88746d2d960a
cristiano3033 <christian300r@gmail.com>
2024-03-03 12:44:44 -0600

    cambio de clase empleado

--- Empleado.cpp
+++ Empleado.cpp
@@ -5,8 +5,17 @@ using namespace std;
 
 class Empleado : Persona{
 	
-	private : Empleado;
-	
 	private : string puesto; 
 	
-}
+	public :
+		Empleado(){
+			
+		}
+		
+		Empleado(string nom, string ape, string dir, int tel, string pst, string fecha_n) : Persona(nom,ape,dir,tel,fecha_n){
+		puesto = pst;			
+			
+		}
+	
+
+};
